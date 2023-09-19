1e9fd94dc493171043f0823d97b61ce38f144486
Simran Ahuja <97106836+SimranAhuja00@users.noreply.github.com>
2023-09-19 21:06:57 +0530

    Create 069. Sqrt(x).cpp

---
+++ 069. Sqrt(x).cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mySqrt(int x) 
+    {
+        long long low = 1, high = x;
+        while(low <= high)
+        {
+            long long mid = (low + high)/2;
+            long long val = mid * mid;
+            if(val <= x)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid-1;
+            }
+        }
+        return high;
+    }
+};

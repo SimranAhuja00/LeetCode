3514c5862a270ed08c028f9aed5a6eef60aed0c1
Simran Ahuja <97106836+SimranAhuja00@users.noreply.github.com>
2023-09-24 14:10:33 +0530

    Update Pow(x,n).java

--- Pow(x,n).java
+++ Pow(x,n).java
@@ -1,16 +1,25 @@
-class Solution {
-    public double myPow(double x, int n) 
-    {
-        if (n == Integer.MAX_VALUE) return x;
-        else if (n == Integer.MIN_VALUE) return (x == 1 || x == -1) ? 1 : 0;
-        if(n<0) 
-        {
-            x=1/x;
-            n*=-1;
+ class Solution {
+    public double myPow(double x, int n) {
+
+        double ans = 1.0;
+        long power = n;
+
+        if(power < 0){
+            power = -1 * power;
         }
-        double ans = 1;
-        for(int i=0;i<n;i++)
-            ans = ans*x;
+
+        while(power > 0){
+            if((power % 2) == 1){
+                ans = ans * x;
+                power -= 1;
+            }else{
+                x = x * x;
+                power = power / 2;
+            }
+        }
+
+        if(n < 0) ans = (double)(1.0) / (double)(ans);
+
         return ans;
-    }    
+    }
 }
